51c49d2a39e5dc3d8bae4caeb5196a3fd3737b23
tiberiu1999 <tiberiuiuliann@yahoo.com>
2020-10-07 21:34:58 +0300

    Initial commit

---
+++ Assets/animation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation : MonoBehaviour
+{
+    public Animation animations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animations = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Dist = Vector3.Distance(Camera.main.transform.position, transform.position);
+        if (Dist < 0.2)
+            animations.Play("atacare");
+        else
+            animations.Play("Death");
+    }
+}
---
+++ Assets/scrip1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    private Animation animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Dist = Vector3.Distance(Camera.main.transform.position, transform.position);
+        if (Dist < 0.3)
+            animation.Play("Death");
+        else
+            animation.Play("atacare");
+    }
+}
